34b2b30ee34e5c606c2aca8d2d4ada1a050b31d4
Wescley Martins Sardinha Teixeira <wescleymartins@brq.com>
2023-06-13 21:10:03 -0300

    Setup do projeto, configuração das entidades e seed do abnco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1994-07-20', 2, '07783690130', 6500.0, 'Augusto Guilherme Santos');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1953-04-18', 1, '90624213005', 2500.0, 'Geraldo Oliver Fogaça');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1984-02-02', 3, '28771592482', 5500.0, 'Samuel Isaac Gonçalves');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1981-01-20', 0, '90374632073', 3000.0, 'Leandro Benjamin Erick Assis');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1972-05-05', 4, '94652393482', 4000.0, 'Carla Antonella Emanuelly Jesus');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1995-05-24', 1, '84084471810', 8500.0, 'Arthur Benedito Nogueira');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1975-02-03', 5, '68002351649', 3500.0, 'Kauê Joaquim Gael Castro');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1998-03-20', 3, '66243769453', 10500.0, 'Andreia Alícia Evelyn Barros');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1978-04-01', 2, '15096428460', 6000.0, 'Calebe Pietro Kaique da Luz');
+INSERT INTO tb_client(birth_date, children, cpf, income, name) VALUES ('1976-02-02', 2, '43144720909', 6500.0, 'Gael Vinicius Brito');
