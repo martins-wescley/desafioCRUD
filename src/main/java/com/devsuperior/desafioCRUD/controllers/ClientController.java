f97a0919dfe8d0d2b6dea41a960786a790ab9a61
Wescley Martins Sardinha Teixeira <wescleymartins@brq.com>
2023-06-14 15:08:05 -0300

    Busca paginada

--- src/main/java/com/devsuperior/desafioCRUD/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafioCRUD/controllers/ClientController.java
@@ -1,16 +1,16 @@
 package com.devsuperior.desafioCRUD.controllers;
 
 import com.devsuperior.desafioCRUD.dto.ClientDTO;
-import com.devsuperior.desafioCRUD.entities.Client;
-import com.devsuperior.desafioCRUD.repositories.ClientRepository;
 import com.devsuperior.desafioCRUD.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -20,8 +20,11 @@ public class ClientController {
     private ClientService service;
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id){
-        ClientDTO dto = service.findById(id);
-        return dto;
+        return service.findById(id);
     }
-
+    
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+    }
 }
--- src/main/java/com/devsuperior/desafioCRUD/services/ClientService.java
+++ src/main/java/com/devsuperior/desafioCRUD/services/ClientService.java
@@ -4,10 +4,12 @@ import com.devsuperior.desafioCRUD.dto.ClientDTO;
 import com.devsuperior.desafioCRUD.entities.Client;
 import com.devsuperior.desafioCRUD.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -18,9 +20,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
-        /*Optional<Client> result = repository.findById(id);
-        Client client = result.get();
-        ClientDTO dto = new ClientDTO(client);
-        return dto;*/
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
